b8419c99eda7c5c3358534d5de97401e9125f62f
peter <pedrosantos2810silva@gmail.com>
2022-06-27 11:06:12 -0400

    alterar nome

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>sou do CV</Text>
+      <Text>aoba</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#763',
+    backgroundColor: '#423',
     alignItems: 'center',
     justifyContent: 'center',
   },
