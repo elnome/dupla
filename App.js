c1a8b31dfb3bbacdaeb9f3fe3d9d8bf7e15bdacb
Gabriel Mendes <biel1422gabriel@gmail.com>
2022-06-27 11:24:38 -0400

    alteração de nome para numeros

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>pikajju</Text>
-      <Text>opa</Text>
+      <Text>opa524645</Text>
       <StatusBar style="auto" />
     </View>
   );
