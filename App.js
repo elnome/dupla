11aa18fee7ac603067a52fe8c376de7e450394b3
peter <pedrosantos2810silva@gmail.com>
2022-06-27 11:24:39 -0400

    altera a cor

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#423',
+    backgroundColor: '#555',
     alignItems: 'center',
     justifyContent: 'center',
   },
