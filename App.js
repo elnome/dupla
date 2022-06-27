30b9d3d59b4a893f84276be4c24e309c7b9eff75
peter <pedrosantos2810silva@gmail.com>
2022-06-27 11:46:20 -0400

    Revert "pedro"
    This reverts commit b8e8acce6ae4dbadb662715293202875424b3e86.

--- App.js
+++ App.js
@@ -4,7 +4,22 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
+<<<<<<< HEAD
       <Text>da um puul ai </Text> 
+=======
+      <Text>pikajju</Text>
+<<<<<<< HEAD
+=======
+      <Text>fala mano</Text>
+      <Text>qual foi macaco</Text>
+      <Text>so de boa?</Text>
+      <Text>tranquilidade uai</Text>
+      <Text>e por ai?</Text>
+      <Text>de boa dms uai</Text>
+      <Text>trabalho da porra </Text>
+      <Text>entt neh zé, tem q ver oq nois vai faze </Text>
+>>>>>>> master
+>>>>>>> parent of b8e8acc... pedro
       <StatusBar style="auto" />
     </View>
   );
