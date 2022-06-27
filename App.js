12fdceec6be336101c2603f8d292dd66d74afcc6
peter <pedrosantos2810silva@gmail.com>
2022-06-27 11:20:16 -0400

    alterar nome

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>aoba</Text>
+      <Text>pikajju</Text>
       <StatusBar style="auto" />
     </View>
   );
