8fe8c524e06b824f93221914fd870aecbe2575bf
peter <pedrosantos2810silva@gmail.com>
2022-06-27 11:45:47 -0400

    pedro

--- App.js
+++ App.js
@@ -4,8 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>da um puul ai </Text>
-      
+      <Text>da um puul ai </Text> 
       <StatusBar style="auto" />
     </View>
   );
